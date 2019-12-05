c03137f6935c3ccb56ae8a9b104d634034ad2bd2
xitengfei <tengfei.xi@qq.com>
2019-12-05 12:54:53 +0800

    Updates

---
+++ precache-manifest.aa6e2cfe2fb757cc677f418d1af96bf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090a7cb8514a9ee5e2011dff412e1c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcea4b92c68c00b82789",
+    "url": "/static/css/2.a0b11396.chunk.css"
+  },
+  {
+    "revision": "675b513d7e2d69ef3c6a",
+    "url": "/static/css/main.59a68c89.chunk.css"
+  },
+  {
+    "revision": "dcea4b92c68c00b82789",
+    "url": "/static/js/2.18c8e4f0.chunk.js"
+  },
+  {
+    "revision": "675b513d7e2d69ef3c6a",
+    "url": "/static/js/main.12107329.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c1ce28d6a251f818c6071419f53df4f",
+    "url": "/static/media/location-icon.2c1ce28d.svg"
+  },
+  {
+    "revision": "e302ab8c99f8890dbb2e1dc7150dd4e3",
+    "url": "/static/media/yikatong-logo-sm.e302ab8c.png"
+  }
+]);
